a353e59c471a1e046ba8d9d4f551698660fcc8cb
Zac Colbert <zcolbert1993@gmail.com>
2023-02-10 14:25:51 -0800

    Add word-based tokenization

--- main.cpp
+++ main.cpp
@@ -9,8 +9,48 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <unordered_map>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+/**
+ * Parse the stream into individual tokens and keep count of the number
+ * of times each token occurs in the document.
+ *
+ * @param fs The text stream to be parsed.
+ * @return A map of tokens and their associated counts for this document.
+ */
+std::unordered_map<std::string, unsigned int> tokenize(std::ifstream& fs)
+{
+    std::unordered_map<std::string, unsigned int> token_counts;
+    std::string token;
+
+    if (fs.is_open()) {
+        while (fs) {
+            fs >> token;
+            if (token_counts.contains(token)) {
+                token_counts[token] += 1;
+            }
+            else {
+                token_counts[token] = 1;
+            }
+        }
+    }
+    else {
+        throw std::runtime_error("Parse error: file stream is invalid");
+    }
+    return token_counts;
+}
+
+
+int main()
+{
+    std::ifstream fs("data/JekyllAndHide.txt");
+    auto tokens = tokenize(fs);
+    for (auto tkn: tokens) {
+        std::cout << tkn.first << ": " << tkn.second << std::endl;
+    }
     return 0;
 }
