b7967e35e396863538adf58e5e861b6cf58c7b19
zcolbert <zcolbert1993@gmail.com>
2023-04-16 13:55:47 -0700

    Refactor likwid csv table parsing

--- scripts/buildreport.py
+++ scripts/buildreport.py
@@ -2,32 +2,8 @@ import argparse
 import csv
 import os
 
+import likwidcsv
 
-class Table:
-    def __init__(self, group, name):
-        self.group = group
-        self.name = name
-        self.rows = list()
-        self.headers = list()
-
-    def add_row(self, row):
-        self.rows.append(row)
-
-    def add_rows(self, rows):
-        self.rows.extend(rows)
-
-    @property
-    def title(self):
-        return f'{self.group} - {self.name}'
-
-    def __eq__(self, rhs):
-        return self.title == rhs.title
-
-    def __hash__(self):
-        return hash(self.title)
-
-    def __repr__(self):
-        return f"Table<group='{self.group}', name='{self.name}', rows={len(self.rows)}>"
 
 
 def load_report_names_from_file(filename):
@@ -64,70 +40,8 @@ def parse_args():
 def main():
     args = parse_args()
 
-    tables = dict()
-    table = None
-
     reports = load_report_names_from_file(args.filename)
-
-    for report in reports:
-
-        # Build an executable label from the name of the report
-        exe = os.path.split(report)[-1].split('.csv')[0]
-
-        with open(report, 'r') as infile:
-            reader = csv.reader(infile)
-
-            for row in reader:
-
-                # Each valid table begins with a delimiting row which has 
-                # the word TABLE in the first column. When this is encountered,
-                # process the existing table (if any) and start consuming rows for 
-                # this new table.
-                if row[0] == 'TABLE':
-                    # start consuming rows
-                    reading = True
-
-                    # If this section marks the end of the previous table, flush it.
-                    if table:
-                        if table.title in tables:
-                            tables[table.title].add_rows(table.rows)
-                        else:
-                            tables[table.title] = table
-
-                        table = None
-
-                    # start a new table
-                    table = Table(group=row[2], name=row[1])
-
-                    row = next(reader)  # discard the delimiting row 
-                    # The next row is the table headers. Add field name to first column.
-                    row.insert(0, 'Executable')  
-                    table.headers = row
-                    continue
-
-                # The report file contains sections that describe the system hardware.
-                # These informational sections begin with a row containing the STRUCT 
-                # keyword in the first column. Ignore any rows belonging to this section.
-                elif row[0] == 'STRUCT':
-                    # ignore any rows until the next table is encountered
-                    reading = False
-
-                    # If this section marks the end of the previous table, flush it.
-                    if table:
-                        if table.title in tables:
-                            tables[table.title].add_rows(table.rows)
-                        else:
-                            tables[table.title] = table
-                        
-                        table = None
-                
-                # If the parser is currently consuming table rows, process the current row.
-                if reading:
-                    row.insert(0, exe)  # insert the executable into the first column
-                    #rows.append(row)
-                    if table:
-                        table.add_row(row)
-    
+    tables = likwidcsv.parse_reports(reports)
 
     # Write the aggregated reports to an output file
     outpath = 'combined.csv'
@@ -135,6 +49,7 @@ def main():
         outpath = args.output
 
     write_combined_file(outpath, tables)
+
 
 
 if __name__ == '__main__':
---
+++ scripts/likwidcsv.py
@@ -0,0 +1,118 @@
+import csv
+import os
+
+from typing import Dict
+
+
+"""Represents a table of LIKWID perfctr data points for a single metric group"""
+class Table:
+    def __init__(self, name, group):
+        self.name = name
+        self.group = group
+        self.rows = list()
+        self.headers = list()
+
+    def add_row(self, row):
+        self.rows.append(row)
+
+    def add_rows(self, rows):
+        self.rows.extend(rows)
+
+    @property
+    def title(self):
+        return f'{self.group} - {self.name}'
+
+    def __eq__(self, rhs):
+        return self.title == rhs.title
+
+    def __hash__(self):
+        return hash(self.title)
+
+    def __repr__(self):
+        return f"Table<group='{self.group}', name='{self.name}', rows={len(self.rows)}>"
+
+    def print(self):
+        print(self.title.upper())
+        print(','.join(self.headers))
+        for row in self.rows:
+            print(','.join(row))
+
+
+def parse_reports(reports):
+    tables = dict()
+
+    for report in reports:
+        # Build an executable label from the name of the report
+        #exe = os.path.split(report)[-1].split('.csv')[0]
+        report_tables = parse_likwid_csv(report)
+        for title, table in report_tables.items():
+            if title in tables:
+                tables[title].add_rows(report_tables[title].rows)
+            else:
+                tables[title] = table
+
+    return tables
+
+
+def parse_likwid_csv(filepath) -> Dict[str, Table]:
+    """Parse out all the performance counter tables in a single CSV file.
+    @param filepath The path to a CSV file containing performance counter data.
+    @return A dictionary of tables, with the titles of each table as the keys.
+    """
+    tables = dict()
+    table = None
+    reading = False
+
+    with open(filepath, 'r') as infile:
+        reader = csv.reader(infile)
+
+        for row in reader:
+            # Each valid table begins with a delimiting row which has
+            # the word TABLE in the first column. When this is encountered,
+            # process the existing table (if any) and start consuming rows for
+            # this new table.
+
+            if row[0] == 'TABLE':
+                reading = True  # start consuming rows
+
+                # If this section marks the end of the previous table, flush it.
+                if table:
+                    if table.title in tables:
+                        tables[table.title].add_rows(table.rows)
+                    else:
+                        tables[table.title] = table
+
+                    table = None
+
+                # start a new table
+                table = Table(name=row[1], group=row[2])
+
+                row = next(reader)  # discard the delimiting row
+                table.headers = row
+                continue
+
+            # The report file contains sections that describe the system hardware.
+            # These informational sections begin with a row containing the STRUCT
+            # keyword in the first column. Ignore any rows belonging to this section.
+            elif row[0] == 'STRUCT':
+                # ignore any rows until the next table is encountered
+                reading = False
+
+                # If this section marks the end of the previous table, flush it.
+                if table:
+                    if table.title in tables:
+                        tables[table.title].add_rows(table.rows)
+                    else:
+                        tables[table.title] = table
+
+                    table = None
+
+            # If the parser is currently consuming table rows, process the current row.
+            if reading:
+                #row.insert(0, exe)  # insert the executable into the first column
+                if table:
+                    table.add_row(row)
+
+    return tables
+
+
